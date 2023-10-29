a3d535dfc824f4570f9744a8b58ac9c72ee90828
jarneamerlinck <jarneamerlinck@gmail.com>
2023-10-29 15:38:55 +0100

    install checking

--- scripts/debian12.sh
+++ scripts/debian12.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkgToInstallListFull="firefox kitty gh sddm neofetch btop htop"
+pkgToInstallListFull="kitty gh sddm neofetch btop htop"
 pkgToRemoveListFull="gnome-weather gnome-maps gnome-contacts aisleriot gnome-sudoku mahjongg ace-of-penguins gnomine gbrainy gnome-mines gnome-2048 gnome-chess five-or-more firefox-esr four-in-a-row yelp hitori gnome-klotski lightsoff gnome-mines gnome-nibbles malcontent seahorse quadrapassel iagno gnome-music gnome-robots shotwell swell-foop synaptic gnome-taquin gedit gnome-todo"
 # Remove terminal bleep
 
@@ -26,7 +26,7 @@ done
 
 
 # install and upgrade stuff stuff
-add-apt-repository ppa:mozillateam/ppa
+#add-apt-repository ppa:mozillateam/ppa
 
 
 
