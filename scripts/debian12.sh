91d44d4ecc82f5d11a88febc5dbe13c7479333d0
jarneamerlinck <jarneamerlinck@gmail.com>
2023-10-29 15:20:52 +0100

    gh apt install

--- scripts/debian12.sh
+++ scripts/debian12.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkgToInstallListFull="firefox kitty gh sddm"
+pkgToInstallListFull="firefox kitty gh sddm neofetch btop htop"
 pkgToRemoveListFull="gnome-weather gnome-maps gnome-contacts aisleriot gnome-sudoku mahjongg ace-of-penguins gnomine gbrainy gnome-mines gnome-2048 gnome-chess five-or-more firefox-esr four-in-a-row yelp hitori gnome-klotski lightsoff gnome-mines gnome-nibbles malcontent seahorse quadrapassel iagno gnome-music gnome-robots shotwell swell-foop synaptic gnome-taquin gedit gnome-todo"
 # Remove terminal bleep
 
@@ -24,9 +24,6 @@ for pkgToRemove in $(echo $pkgToRemoveListFull); do
   fi
 done
 
-apt-get --yes --purge remove $pkgToRemoveList
-
-apt autoremove
 
 # install and upgrade stuff stuff
 add-apt-repository ppa:mozillateam/ppa
@@ -35,5 +32,10 @@ add-apt-repository ppa:mozillateam/ppa
 
 apt update
 apt install -y $pkgToInstallListFull 
+
+apt-get --yes --purge remove $pkgToRemoveList
+
+apt autoremove
+
 
 echo "Fully run"
