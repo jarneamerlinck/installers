9d17a1067a520c533c2ba81d8d3bbd0064b4c9da
jarneamerlinck <jarneamerlinck@gmail.com>
2023-10-29 14:33:14 +0100

    update_testing

--- scripts/debian12.sh
+++ scripts/debian12.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-apt update && apt upgrade -y
+# Remove terminal bleep
+
+sed -i "set bell-style none/c\set bell-style none" /etc/inputrc
+
+
+# update
+#apt update 
+
+
+
+# remove unused gnome apps
+
+#apt remove --purge gnome-weather  gnome-maps gnome-contacts aisleriot gnome-sudoku mahjongg ace-of-penguins gnomine gbrainy gnome-mines
+
+
+# upgrade stuff
